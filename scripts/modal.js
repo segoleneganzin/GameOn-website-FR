1442cd289bd9a22ea660d3d3090231f7e7bf0f45
Ségolène Ganzin <segolene.ganzin@gmail.com>
2023-10-23 10:27:17 +0200

    responsive

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,12 +4,21 @@ const modalBtn = document.querySelectorAll(".modal__btn");
 const modalBg = document.querySelector(".modal__bground");
 const modalClose = document.querySelector(".modal__close");
 const formData = document.querySelectorAll(".modal__formData");
+const numTournaments = document.getElementById("num-tournaments");
 
 // **************** global variables
+let screenSize = window.matchMedia("(min-width: 475px)");
 
 // **************** page load function for events
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("HTML prêt !");
+  // replace text if media screen is under 475px
+  if (screenSize.matches) {
+    numTournaments.innerHTML =
+      "À combien de tournois GameOn avez-vous déjà participé ?";
+  } else {
+    numTournaments.innerHTML =
+      "À combien de tournois avez-vous déjà participé ?";
+  }
   // **** events
   // launch modal event
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
